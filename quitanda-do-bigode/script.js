76c14fdfa60a43e7342858d029054d9d973afe7c
BrunoLeon007 <brunob734@gmail.com>
2024-11-25 21:22:09 -0300

    Add files via upload

---
+++ quitanda-do-bigode/script.js
@@ -0,0 +1,34 @@
+const inicioPrograma = Number(prompt(`
+         ===========Bem-Vindo==============
+               O que deseja comprar?
+            =============================
+               Selecione uma opção
+            _____________________________
+    [1] - Banana
+    [2] - Maçã
+    [3] - Manga
+    [4] - Laranja
+    [5] - Limão
+    
+    `))
+    switch(inicioPrograma) {
+        case 1:
+            alert("banana")
+            Image()
+            break;
+        case 2:
+            alert("Maçã")
+            break;
+            case 3:
+            alert("Manga")
+            break;
+            case 4:
+                ("Laranja")
+            break;
+            case 5:
+                ("Limão")
+                break;
+            default:
+                alert("Não existe esta opção!")
+       
+    }
